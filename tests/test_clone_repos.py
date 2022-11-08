54f5bb2688ca9a756f41c972671e13f4281a39d4
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-08 10:53:02 -0800

    add pytest, basic config test

---
+++ tests/test_clone_repos.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path, PosixPath
+from clone_repos.repo import Repo, parse_file
+
+os.environ["REPOS"] = str(Path.home() / "Repos")
+
+this_dir = Path(__file__).parent
+conf = this_dir / "clone-repos.yaml"
+assert conf.exists()
+
+base = Path(os.environ["REPOS"])
+
+
+def test_parse_config() -> None:
+    repos = parse_file(base=base, file=conf)
+    assert isinstance(repos, list)
+    pl = repos[3]
+    assert isinstance(pl, Repo)
+    assert pl.base == base
+    assert pl.git_url == "https://github.com/seanbreckenridge/bleanser-fork"
+    assert pl.postinstall_cmd == [
+        'git remote add upstream "https://github.com/karlicoss/bleanser"',
+        "git pull upstream master",
+    ]
+    assert pl.pip_install == False
+    assert pl.editable_install == True
+    assert pl.editable_non_user == False
+    assert pl.dirname == "bl-fork"
+    assert pl.symlink_to == "/tmp"
